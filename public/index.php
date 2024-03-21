7a791798cdefac1fb7f1720a9fa2c2679cca95ab
Pablo Gerez <pgerez@gmail.com>
2024-03-21 12:20:39 -0300

    prueba 2 de trusted proxies         modificados:     config/packages/framework.yaml 	modificados:     public/index.php

--- public/index.php
+++ public/index.php
@@ -22,6 +22,21 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+// tell Symfony about your reverse proxy
+Request::setTrustedProxies(
+// the IP address (or range) of your proxy
+    ['10.1.1.5'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+
+// or, if your proxy instead uses the "Forwarded" header
+// Request::HEADER_FORWARDED
+
+// or, if you're using AWS ELB
+// Request::HEADER_X_FORWARDED_AWS_ELB
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
