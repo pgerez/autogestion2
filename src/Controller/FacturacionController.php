e71647b6d06031a44f8583ada36dcc41eea79cb9
Pablo Gerez <pgerez@gmail.com>
2024-05-23 01:49:43 -0300

    produccion = true 	modificados:     src/Controller/FacturacionController.php

--- src/Controller/FacturacionController.php
+++ src/Controller/FacturacionController.php
@@ -76,7 +76,7 @@ class FacturacionController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $montoFact = $entityManager->getRepository(ItemPrefacturacion::class)->findTotalItems($check);
 
-            $afip = new Afip(array('CUIT' => $_ENV['CUIT'], 'production' => $_ENV['PRODUCCION'])); //Reemplazar el CUIT
+            $afip = new Afip(array('CUIT' => $_ENV['CUIT'], 'production' => TRUE)); //Reemplazar el CUIT
             /**
              * Numero del punto de venta
              **/
