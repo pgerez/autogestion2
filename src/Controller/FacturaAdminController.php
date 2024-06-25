d9beb30a63abf3967a8c30204c66b300d80270a7
Pablo Gerez <pgerez@gmail.com>
2024-06-25 12:17:48 -0300

    icheck modal 	modificados:     src/Controller/FacturaAdminController.php 	modificados:     src/Resources/views/Form/formfield_item_widget.html.twig 	modificados:     templates/facturacion/list.html.twig

--- src/Controller/FacturaAdminController.php
+++ src/Controller/FacturaAdminController.php
@@ -18,16 +18,17 @@ final class FacturaAdminController extends CRUDController{
 
     public function listitemsAction(Request $request) : Response
     {
-        $em = $this->getDoctrine()->getManager();
-        $data1 = $request->getContent();
-        #$data = str_replace('"', '', $array->{'dni'});
-        $data = json_decode($data1);
-        $idfactura = $data->idfactura;
-        $idpago = $data->idpago;
-        $idcuota = $data->idcuota;
-        $items = $em->getRepository(Factura::class)->find($idfactura)->getItemPrefacturacions();
-        $cuota = $em->getRepository(Cuota::class)->find($idcuota);
-        $url = $this->generateUrl(
+        $em         = $this->getDoctrine()->getManager();
+        $data1      = $request->getContent();
+        #$data      = str_replace('"', '', $array->{'dni'});
+        $data       = json_decode($data1);
+        $idfactura  = $data->idfactura;
+        $idpago     = $data->idpago;
+        $idcuota    = $data->idcuota;
+        $id         = $data->id;
+        $items      = $em->getRepository(Factura::class)->find($idfactura)->getItemPrefacturacions();
+        $cuota      = $em->getRepository(Cuota::class)->find($idcuota);
+        $url        = $this->generateUrl(
             'admin_app_factura_saveitems',
             array(),
             UrlGeneratorInterface::ABSOLUTE_URL
@@ -39,12 +40,12 @@ final class FacturaAdminController extends CRUDController{
                 <input type="hidden" name="idcuota" value="'.$idcuota.'" />
                 <div class="box-body table-responsive no-padding">
                 <table class="table table-bordered table-striped table-hover sonata-ba-list">
-                <thead>
+                    <tbody>
                     <tr>
                         <td colspan="7" style="text-align: center">Factura: '.$em->getRepository(Factura::class)->find($idfactura)->getNumeroCompleto().'<br> ID:'.$idcuota.'</td>
                     </tr>
                     <tr>
-                        <th>
+                        <th class="sonata-ba-list-field-header sonata-ba-list-field-header-batch">
                             <div class="icheckbox_square-blue" style="position: relative;">
                                 <input type="checkbox" id="list_batch_checkbox" style="position: absolute; opacity: 0;">
                             </div>
@@ -55,8 +56,7 @@ final class FacturaAdminController extends CRUDController{
                         <th>Practica</th>
                         <th>Cantidad</th>
                         <th>Precio</th>
-                    </tr>
-                </thead>';
+                    </tr>';
                 foreach ($items as $item):
                     $cid = is_array($item->getCuota()) ? $item->getCuota()->getId() : 0;
                     $checked = $item->getEstadoPago() == 1 ? 'checked' : '';
@@ -81,6 +81,7 @@ final class FacturaAdminController extends CRUDController{
                             </tr>';
                 endforeach;
          $html .= <<<EOF
+                 </tbody>
                  </table>
                  </div>
                  </div>
@@ -104,6 +105,7 @@ final class FacturaAdminController extends CRUDController{
                                     $(this).closest('tr, div.sonata-ba-list-field-batch').toggleClass('sonata-ba-list-row-selected', $(this).is(':checked'));
                                 }).trigger('ifChanged');
                                 });
+                                Admin.setup_icheck(jQuery('#{$id}'));
                     </script>
                      
 EOF;
