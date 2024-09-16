1f60313450addd0d0b38ce6df8f15330066c5843
Pablo Gerez <pgerez@gmail.com>
2024-09-16 03:46:04 -0300

    fecha cae 	modificados:     src/Admin/FacturaAdmin.php

--- src/Admin/FacturaAdmin.php
+++ src/Admin/FacturaAdmin.php
@@ -128,7 +128,7 @@ final class FacturaAdmin extends AbstractAdmin
             #->add('digitalMonto')
             #->add('fechaEnvioSuper')
             ->add('cae')
-            ->add('cae_vto')
+            ->add('cae_vto', null, ['format'=>'d/m/y'])
             ->add('estadoId', null,['label' => 'Estado']);
         if ($this->isGranted('ROLE_AUTOGESTION') or $this->isGranted('ROLE_SUPER_ADMIN')):
             $list->add('tipoFact', null, ['label' => 'Cert Deuda', 'template' => 'factura/progress.html.twig']);
