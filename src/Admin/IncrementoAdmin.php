1c694d5ac1c178bb972135b5c83dbf0a5c2827c1
Pablo Gerez <pgerez@gmail.com>
2024-10-06 19:38:15 -0300

    hospital en incremento y afectacion 	modificados:     src/Admin/IncrementoAdmin.php

--- src/Admin/IncrementoAdmin.php
+++ src/Admin/IncrementoAdmin.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
+use App\Entity\Hospital;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\Form\Type\DatePickerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 final class IncrementoAdmin extends AbstractAdmin
 {
@@ -45,8 +49,44 @@ final class IncrementoAdmin extends AbstractAdmin
 
     protected function configureFormFields(FormMapper $form): void
     {
+        $sqlh = false;
+        $id   = null;
+        $user = $this->getConfigurationPool()->getContainer()->get('security.token_storage')->getToken()->getUser();
+        if($this->isGranted('ROLE_HPGD') and !$this->isGranted('ROLE_SUPER_ADMIN')){
+            $id = $user->getHospital()->getId();
+            $sqlh= true;
+        }
         $form
             #->add('id')
+            ->ifTrue($sqlh)
+            ->add('hospital', EntityType::class, [
+                'class' => Hospital::class,
+                'choice_value' => 'id',
+                'by_reference' => false,
+                'multiple' => false,
+                'expanded' => false,
+                'required' => true,
+                'query_builder' => function (EntityRepository $er) use ($id) : QueryBuilder {
+                    return $er->createQueryBuilder('h')
+                        ->Where('h.id = :id')
+                        ->setParameter('id', $id);
+                },
+            ])
+            ->ifEnd()
+            ->ifFalse($sqlh)
+            ->add('hospital', EntityType::class, [
+                'class' => Hospital::class,
+                'choice_value' => 'id',
+                'by_reference' => false,
+                'multiple' => false,
+                'expanded' => false,
+                'required' => false,
+                'query_builder' => function (EntityRepository $er) : QueryBuilder {
+                    return $er->createQueryBuilder('h')
+                        ->Where('h.hpgd = 0');
+                },
+            ])
+            ->ifEnd()
             ->add('importe')
             ->add('numero_expediente')
             ->add('fecha')
