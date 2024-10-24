5aa086a888e85a8e9d904c6bac64e5b5d760df12
Pablo Gerez <pgerez@gmail.com>
2024-10-23 23:16:55 -0300

    para envio por multiple smtp 	modificados:     src/Controller/FacturaAdminController.php

--- src/Controller/FacturaAdminController.php
+++ src/Controller/FacturaAdminController.php
@@ -913,6 +913,7 @@ EOF;
                 ->html('Se envía factura de prestaciones médico realizada en el establecimiento asistencial de referencia en la Pcia de Santiago del Estero conforme a la Ley Pcial N 7384/2024')
                 ->attach($fpdf, 'factura'.$factura.'.pdf')
                 ->getHeaders()->addTextHeader('X-Transport', $efector);
+            $mailer->getTransport($this->getUser()->getHospital()->getCodigoh())->send($email);
         }else{
             $email = (new Email())
                 ->from($_ENV['EMAIL'])
@@ -925,9 +926,10 @@ EOF;
                 ->text('Se envía factura de prestaciones médico realizada en el establecimiento asistencial de referencia en la Pcia de Santiago del Estero conforme a la Ley Pcial N 7384/2024')
                 ->html('Se envía factura de prestaciones médico realizada en el establecimiento asistencial de referencia en la Pcia de Santiago del Estero conforme a la Ley Pcial N 7384/2024')
                 ->attach($fpdf, 'factura'.$factura.'.pdf');
+            $mailer->send($email);
         }
 
-        $mailer->send($email);
+
 
         #guardo fecha de envio#####
         $factura->setFechaEnvio(new \DateTime());
